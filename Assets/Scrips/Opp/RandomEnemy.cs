2eb668454851b7dcc21560154d1deaebacd0a8f2
Nicolas07n <nicolasmg4012@gmail.com>
2025-06-10 19:52:00 +0200

    Cambios de texto
    cambio

--- Assets/Scrips/Opp/RandomEnemy.cs
+++ Assets/Scrips/Opp/RandomEnemy.cs
@@ -2,17 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RandomEnemy : MonoBehaviour
+public class RandomEnemy : Enemy
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector2 dir;
+    private float timeToSpin;
+    private float currentTime;
+    private bool confused;
+
+    public RandomEnemy(Rigidbody2D rb, Vector2 dir, float timeToSpin) : base(10f, rb, null)
     {
-        
+        this.dir = dir;
+        this.timeToSpin = timeToSpin;
+        this.currentTime = 0f;
+        this.confused = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Move()
     {
-        
+        if (confused)
+        {
+            rb.velocity = -dir * speed;
+        }
+        else
+        {
+            rb.velocity = dir * speed;
+        }
+    }
+
+    public override void Hit()
+    {
+        confused = true;
+        rb.transform.Rotate(Vector3.forward * 360f);
+        Invoke("EndConfusion", timeToSpin);
+    }
+
+    private void EndConfusion()
+    {
+        confused = false;
     }
 }
